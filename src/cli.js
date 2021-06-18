7a66aa43f19a10b596161f397350428ea9e0e9cb
Vlad Ivankov <vlad.ivankov.96@mail.ru>
2021-06-18 22:56:43 +0400

    5

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,7 @@
 import readlineSync from 'readline-sync';
 
- export const sayHello = () =>{
-	const name = readlineSync.question('May I have your name?');
-	console.log(`Hello, ${name}!`)
-}
-
-
+const sayHello = () => {
+  const name = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${name}!`);
+};
+export default sayHello;
