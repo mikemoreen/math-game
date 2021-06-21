d1014d0245e58252046400d3179121d26e049c0f
Vlad Ivankov <vlad.ivankov.96@mail.ru>
2021-06-22 03:24:44 +0400

    4

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 import greeting from '../src/cli.js';
-import { isEven, playGame} from '../src/brain-even-logic.js';
+import {playGame} from '../src/brain-even-logic.js';
 
-const name = greeting()
-const countOfGames = 3
-console.log(playGame(countOfGames,name))
+
+
+
+
+playGame()
--- src/brain-even-logic.js
+++ src/brain-even-logic.js
@@ -1,36 +1,47 @@
 
 import readlineSync from 'readline-sync';
 import { getRandom } from "./random.js"
-
+import greeting from './cli.js';
 
 export const isEven = (num) => num%2===0?"yes":"no"
 
-export const playGame = (countOfGames,name) =>{
-    if(countOfGames===0){
-        return `Congratulations, ${name}`
-    }
-    const randomNum = getRandom()
-    console.log(`Question: ${randomNum}`)
-    let answer = readlineSync.question("Your answer:")
-    if(answer!=="no" && answer!=="yes"){
-        answer ="no"
-    }
+export const playGame = () =>{
+    
+    let name;
+    for(let i = 0; i<=3; i+=1){
+        if (i===0){
+            name = greeting()
+        }
+        if(i===3){
+            return `Congratulations, ${name}`
+        }
+        const randomNum = getRandom()
+        console.log(`Question: ${randomNum}`)
+        let answer = readlineSync.question("Your answer:")
+        if(answer!=="no" && answer!=="yes"){
+            answer ="no"
+        }
 
 
     
-     if (isEven(randomNum)==="yes" && answer ==="no"){
-        
-        return "'no' is wrong answer ;(. Correct answer was 'yes'."
-    }
-    else if (isEven(randomNum)==="no" && answer ==="yes"){
-         
-         return "'yes' is wrong answer ;(. Correct answer was 'no'."
-    }
-    else{
-        console.log("Correct!")
-        return playGame(countOfGames-1,name)
-    }
+        if (isEven(randomNum)==="yes" && answer ==="no"){
+            
+            return "'no' is wrong answer ;(. Correct answer was 'yes'."
+        }
+        else if (isEven(randomNum)==="no" && answer ==="yes"){
+            
+            return "'yes' is wrong answer ;(. Correct answer was 'no'."
+        }
+        else{
+            console.log("Correct!")
+            
+        }    
 
+    }
+    
+    
+    
+    
 
 
 }
