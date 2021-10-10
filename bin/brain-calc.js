b4c24f41753d3dda0a6861defa1372cd83be93e2
Vlad Ivankov <vlad.ivankov.96@mail.ru>
2021-10-10 23:07:34 +0400

    fix

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import { playGame } from '../src/games/brain-calc-logic.js';
+import  playGame  from '../src/games/brain-calc-logic.js';
 
 playGame();
--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import { playGame } from '../src/games/brain-even-logic.js';
+import  playGame  from '../src/games/brain-even-logic.js';
 
 playGame();
